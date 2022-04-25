ef1eee2daa8f35dbac510de5ba42ed0fe1aeb7c4
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-25 15:06:55 +0100

    Add route to create a new publisher

--- controllers/publishers_controller.py
+++ controllers/publishers_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
 from repositories import publisher_repository
+from models.publisher import Publisher
 
 publishers_blueprint = Blueprint("publishers", __name__)
 
@@ -7,3 +8,18 @@ publishers_blueprint = Blueprint("publishers", __name__)
 def publishers():
     publishers = publisher_repository.select_all()
     return render_template("publishers/index.html.jinja", publishers = publishers)
+
+
+@publishers_blueprint.route("/publishers/new")
+def new():
+    return render_template("publishers/new.html.jinja")
+
+@publishers_blueprint.route("/publishers", methods=["POST"])
+def create():
+    name = request.form["name"]
+    country = request.form["country"]
+    address = request.form["address"]
+
+    publisher_repository.add(Publisher(name, country, address))
+
+    return redirect("/publishers")
