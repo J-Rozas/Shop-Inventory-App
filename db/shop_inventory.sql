261f842f6e84ca550504ae323f6102bfafece33f
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-25 11:15:23 +0100

    Add database schema

---
+++ db/shop_inventory.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS publishers;
+
+CREATE TABLE publishers(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    country VARCHAR(100),
+    address VARCHAR(100)
+);
+
+CREATE TABLE books(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    number_of_pages INT,
+    genre VARCHAR(100),
+    author VARCHAR(100),
+    stock INT,
+    selling_price FLOAT,
+    publisher_id SERIAL REFERENCES publishers(id)
+);
