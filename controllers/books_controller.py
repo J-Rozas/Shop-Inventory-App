8058de1672b20ea2a56e5f3c2559ab6ea9bba2da
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-25 20:40:34 +0100

    Update book controller and add index template for books

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from repositories import book_repository
 
 books_blueprint = Blueprint("books", __name__)
 
 @books_blueprint.route("/books")
 def books():
-    return render_template("books/index.html.jinja")
+    books = book_repository.select_all()
+    return render_template("books/index.html.jinja", books = books)
