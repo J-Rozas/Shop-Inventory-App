49481492a945963b0299eb74012e101f9e54b453
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-25 14:01:33 +0100

    Update controller to use relevant repository

--- controllers/publishers_controller.py
+++ controllers/publishers_controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from repositories import publisher_repository
 
 publishers_blueprint = Blueprint("publishers", __name__)
 
 @publishers_blueprint.route("/publishers")
 def publishers():
-    return render_template("publishers/index.html.jinja")
+    publishers = publisher_repository.select_all()
+    return render_template("publishers/index.html.jinja", publishers = publishers)
