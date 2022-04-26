b013f2ed1e04221b9870448ea8771a869f806a4f
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-26 12:47:44 +0100

    Add routes to sell a book

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -30,3 +30,32 @@ def create():
     book_repository.add(Book(title, number_of_pages, genre, author, stock, publisher, selling_price))
 
     return redirect("/books")
+
+@books_blueprint.route("/books/<id>/sell")
+def edit(id):
+    book = book_repository.select(id)
+    return render_template("books/sell.html.jinja", book = book)
+
+
+@books_blueprint.route("/books/<id>", methods=["POST"])
+def update(id):
+    units_to_sell = int(request.form["stock"])
+
+    current_book = book_repository.select(id)
+
+    title = current_book.title
+    number_of_pages = current_book.number_of_pages
+    genre = current_book.genre
+    author = current_book.author
+    stock = int(current_book.stock)
+    publisher = current_book.publisher
+    selling_price = current_book.selling_price
+
+    if stock < units_to_sell:
+        new_stock = 0
+    else:
+        new_stock = stock - units_to_sell
+
+    book_repository.update(Book(title, number_of_pages, genre, author, new_stock, publisher, selling_price, id))
+
+    return redirect("/books")
