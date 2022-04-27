7596453d7bfc50100c8bef7b6587161ebda60e5b
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-27 19:19:36 +0100

    Add more books to populate the database

--- populate_db.py
+++ populate_db.py
@@ -15,11 +15,20 @@ publisher_repository.add(publisher2)
 book1 = Book("Ulysses", 730, "Modernist novel", "James Joyce", 10, publisher1, 35.99)
 book_repository.add(book1)
 
-book2 = Book("The Great Gatsby", 163, "Historical fiction", "F. Scott Fitzgerald", 6, publisher2, 17.85)
+book2 = Book("The Great Gatsby", 163, "Historical fiction novel", "F. Scott Fitzgerald", 6, publisher2, 17.85)
 book_repository.add(book2)
 
 book3 = Book("To the Lighthouse", 320, "Fiction novel", "Virginia Woolf", 3, publisher1, 16.99)
 book_repository.add(book3)
 
-book4 = Book("Nineteen Eighty-Four", 328, "Science fiction", "George Orwell", 0, publisher2, 10.00)
+book4 = Book("Nineteen Eighty-Four", 328, "Science fiction novel", "George Orwell", 0, publisher2, 10.49)
 book_repository.add(book4)
+
+book5 = Book("The Grapes of Wrath", 464, "Historical fiction novel", "John Steinbeck", 1, publisher2, 9.99)
+book_repository.add(book5)
+
+book6 = Book("Lolita", 360, "Fiction novel", "Vladimir Nabovok", 0, publisher1, 40.05)
+book_repository.add(book6)
+
+book7 = Book("On the Road", 366, "Fiction novel", "Jack Kerouac", 14, publisher2, 23.96)
+book_repository.add(book7)
