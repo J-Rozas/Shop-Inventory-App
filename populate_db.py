e887534a063085b342664c51ddcf8887e4cd380a
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-27 15:25:59 +0100

    Modify initial publishers

--- populate_db.py
+++ populate_db.py
@@ -6,10 +6,10 @@ from repositories import publisher_repository, book_repository
 book_repository.delete_all()
 publisher_repository.delete_all()
 
-publisher1 = Publisher("Penguin Random House", "United States", "1745 Broadway, New York, NY 10019, United States")
+publisher1 = Publisher("Penguin Random House", "United States", "1745 Broadway, New York, NY 10019")
 publisher_repository.add(publisher1)
 
-publisher2 = Publisher("Hachette Livre", "France", "8 Rue d'Assas, 75006 Paris, France")
+publisher2 = Publisher("Hachette Livre", "France", "8 Rue d'Assas, 75006 Paris")
 publisher_repository.add(publisher2)
 
 book1 = Book("Ulysses", 730, "Modernist novel", "James Joyce", 10, publisher1, 35.99)
